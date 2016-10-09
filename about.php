467254b6f3aca2477f5796706844b2a7dfd004bd
Antun K550J <antun.matanovic@gmail.com>
2016-10-09 18:31:50 +0200

    Dodan opis

--- about.php
+++ about.php
@@ -1,36 +1,34 @@
 <div class="about" id="about">
     <div class="row">
         <div class="large-12 columns opis">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis varius a urna eget fermentum. Curabitur dictum ut quam vel fermentum.
-                Donec laoreet turpis in leo viverra lobortis. Pellentesque pretium tincidunt justo in eleifend.
-                Vivamus et libero ante. In non tempor lorem, id commodo justo. Ut a quam bibendum, laoreet dui.
+            <p>
+                Diplomski rad na temu Razvoj programskog rješenja za mapiranje shema metapodataka, izrađen 2016. godine
+                omogućuje automatsko mapiranje iz Dublin Core sheme u MODS i obrnuto. Potrebno je samo priložiti datoteke
+                i kada se mapiranje završi, dobit ćete poveznicu za preuzimanje dokumenata. Aplikacija će prije procesa mapiranja
+                provjeriti valjanost prema XSD shemi i ukoliko dokument ima pogrešaka, reći će točno što nije ispravno. Prilikom
+                mapiranja se konzultiraju normativne datoteke osobnih imena Kongresne knjižnice te ISO standardi za kodove država i jezika.
             </p>
         </div>
     </div>
 <div class="row">
-<div class="large-4 columns ">
- <div class="theme_button" style="background: #009688">
- 	<i class="fa fa-lightbulb-o"></i>
- </div>
-<p>Aplikacija je u mogućnosti sama prepoznati radi li se o MODS ili Dublin Core shemi metapodataka,
-stoga priložene datoteke moraju imati definirane imenske prostore.
-</p>
+
+    <div class="large-10 columns push-2">
+        <p>Aplikacija je u mogućnosti sama prepoznati radi li se o MODS ili Dublin Core shemi metapodataka,
+            stoga priložene datoteke moraju imati definirane imenske prostore.
+        </p>
+    </div>
 </div>
-<div class="large-4 columns">
+    <div class="row">
+    <div class="large-2 columns">
  <div class="theme_button" style="background: #009688">
- 	<i class="fa fa-exclamation"></i>
+ 	<i class="fa fa-info"></i>
  </div>
+        </div>
+        <div class="large-10 columns">
 <p>Maksimalan broj datoteka koje se mogu priložiti je pet i sve moraju biti u XML formatu. Moguće je u istom trenutku priložiti 
 različite sheme metapodataka.
 </p> 
 </div>
-<div class="large-4 columns">
- <div class="theme_button" style="background: #009688">
- 	<i class="fa fa-info"></i>
- </div>
-<p>Tijekom mapiranja aplikacija izvještava o trenutnom napretku, a nakon završetka ispisuje status priloženih datoteka i daje poveznicu
-za preuzimanje.
-</p>
-</div>
-</div>
+        </div>
+
 </div>
